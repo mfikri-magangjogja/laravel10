b1a5bc56ab4dfebb8f566fd826448964bfe486f6
dzaki201 <mfikri.magangjogja@gmail.com>
2024-08-03 14:49:44 +0700

    coomit pertama

---
+++ database/migrations/2024_08_03_072429_t_kaprodi.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_kaprodi', function (Blueprint $table) {
+            $table->id('kaprodi_id');
+            $table->foreignId('user_id')->unique()->constrained('users', 'user_id');
+            $table->string('nama', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_kaprodi');
+    }
+};
